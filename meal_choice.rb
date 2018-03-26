2fdc81c3fcc4095eaa9fa0d2a4928d7eb25e29df
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-03-26 22:40:58 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,6 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal_choice("meat")
-  puts "I like #{meat}"
+def meal_choice("vegan")
+  puts meal_choice
 end
